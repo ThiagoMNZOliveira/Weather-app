68936dd25ba89325e47e508b878316b1a1bfa2b6
thiagomnzoliveira <thiagomodev@gmail.com>
2023-04-14 21:51:23 -0300

    remove bigger container styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import './App.css'
 
 // components
 import Temperature from './components/Temperature/Temperature'
+import DayTemperature from './components/DayTemperature/DayTemperature'
+import WeekTemperature from './components/WeekTemperature/WeekTemperature'
+import Search from './components/Search/Search'
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      <Temperature />
+      <div className='container'>
+        <Search />
+        <Temperature />
+        <DayTemperature />
+        <WeekTemperature />
+      </div>
     </div>
   )
 }

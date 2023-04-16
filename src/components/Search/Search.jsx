7ba36bf84828a138617f00a41356da7ad785ab21
thiagomnzoliveira <thiagomodev@gmail.com>
2023-04-16 00:08:45 -0300

    Search component with async paginate

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,38 +1,72 @@
+import { useState } from 'react'
+import { AsyncPaginate } from 'react-select-async-paginate'
+
+import { GEO_API_URL, geoAPIOptions } from '../../api'
+
+
 import { MdSearch } from 'react-icons/md'
 import './Search.css'
-import { useState } from 'react'
-
-const Search = () => {
-
-    const [city, setCity] = useState('');
 
 
-    const handleChange = (e) => {
-        setCity(e.target.value)
-    }
+const Search = ({ onSearchChange }) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(city)
+    const [search, setSearch] = useState(null);
+
+    const handleChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+
     }
 
 
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoAPIOptions)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    }
+
+
+    const customStyles = {
+        control: (provided, state) => ({
+            ...provided,
+            width: '380px',
+            height: '40px',
+            borderRadius: '15px',
+            border: '2px solid #ccc',
+            boxShadow: state.isFocused ? '0 0 0 2px #3699FF' : null,
+            
+        }),
+        option: (provided, state) => ({
+            ...provided,
+            backgroundColor: state.isFocused ? '#3699FF' : null,
+            color: state.isFocused ? 'white' : null,
+        
+        }),
+    }
 
     return (
         <div className='search-container'>
 
-            <input
-                type="text"
-                id='city'
-                name='city'
+            <AsyncPaginate
+                placeholder='Search for city'
+                debounceTimeout={600}
+                value={search}
                 onChange={handleChange}
-                value={city}
-                placeholder="Search city"
+                loadOptions={loadOptions}
+                styles={customStyles}
             />
-            <button type='submit' onClick={handleSubmit}><MdSearch /></button>
-
-
         </div>
+
     )
 }
 

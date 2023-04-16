fad9f6f32afe9d165c40c83026536e98a7691e99
thiagomnzoliveira <thiagomodev@gmail.com>
2023-04-16 00:10:32 -0300

    change current wheather component styles

--- src/components/Temperature/Temperature.jsx
+++ src/components/Temperature/Temperature.jsx
@@ -1,17 +1,19 @@
 import './Temperature.css'
 import { IoLocationSharp } from 'react-icons/io5'
-import { BsFillSunFill } from 'react-icons/bs'
 
-const Temperature = () => {
+const Temperature = ({ cityName }) => {
   return (
     <div className='temperature-container'>
       <div className='info'>
-        <span className='temperature'>30º</span>
-        <span className='localization'>Algum Lugar <IoLocationSharp className='IoLocationSharp'></IoLocationSharp></span>
-        <span className='feeling'>31º / 25° Sensação térmica de 34°</span>
+        <div>
+          <p className='localization'>{cityName}<IoLocationSharp className='IoLocationSharp'></IoLocationSharp></p>
+          <p className='description'>Sunny</p>
+        </div>
+        <p className='temperature'>30ºC</p>
+        <p className='feeling'>31º / 25° Sensação térmica de 34°</p>
       </div>
       <div className='icon'>
-        <BsFillSunFill />
+        <img alt='wheather' className='wheather-icon' src='icons/01d.png' />
       </div>
     </div>
   )

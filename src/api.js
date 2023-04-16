916685d67c46c0ed4492d47e07b0409ac4c3bcb7
thiagomnzoliveira <thiagomodev@gmail.com>
2023-04-16 00:06:57 -0300

    add api file

---
+++ src/api.js
@@ -0,0 +1,16 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a543936b67msh53e5158b6c79072p1c409djsn1a622bdd1ccd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+export const WHEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WHEATHER_API_KEY = '8a3c4225cf9f310a217bf43a8f810ea5'
+

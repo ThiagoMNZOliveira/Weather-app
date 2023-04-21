426b1b3d83f7b2a3a2f31e02024e39e2009ff9ee
thiagomnzoliveira <thiagomodev@gmail.com>
2023-04-20 22:05:00 -0300

    add forecast data

--- src/components/DayTemperature/DayTemperature.jsx
+++ src/components/DayTemperature/DayTemperature.jsx
@@ -1,8 +1,22 @@
 import './DayTemperature.css'
+import { IoIosWater } from 'react-icons/io'
 
-const DayTemperature = () => {
+const DayTemperature = ({ data }) => {
   return (
-    <div className='day-temperature-container'>DayTemperature</div>
+    <div className='day-temperature-container'>
+      {data.list.map((item, index) => {
+        return index >= 10 ? null : (
+          <div key={index} className='forecast-container'>
+            <p className='time'>{item.dt_txt.slice(-8)}</p>
+            <div className='icon'>
+              <img alt='weather icon' className='weather-icon' src={`icons/${item.weather[0].icon}.png`} />
+            </div>
+            <p className='temp'>{Math.round(item.main.temp)}º</p>
+            <p className='humidity'><IoIosWater/> {item.main.humidity}%</p>
+          </div>)
+      })
+      }
+    </div >
   )
 }
 
